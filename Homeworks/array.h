ade4d19cd65dcc3e7af2a730d38acd07cf521b4e
Tatev <tatevikyeghiazaryann5@gmail.com>
2025-10-09 14:47:46 +0400

    first commit

---
+++ Homeworks/array.h
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 9/22/2025.
+//
+
+#ifndef UNTITLED_ARRAY_H
+#define UNTITLED_ARRAY_H
+
+#endif //UNTITLED_ARRAY_H
---
+++ Homeworks/array1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ostream>
+
+int cin;
+//
+//
+int main() {
+    int array1[10];
+    std::cout<< "Write array 1 ";
+    std::cout << array1[0] << std::endl;
+    std::cout << array1[1] << std::endl;
+    std::cout << array1[2] << std::endl;
+    std::cout << array1[3] << std::endl;
+}
