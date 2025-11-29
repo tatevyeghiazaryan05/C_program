ae27cc79d7269095254d1e4eda1e492c7bd37124
Tatev <tatevikyeghiazaryann5@gmail.com>
2025-11-29 16:53:35 +0400

    explicit

---
+++ Homeworks/Class/explicit.cpp
@@ -0,0 +1,72 @@
+// #include <iostream>
+// class B {
+// public:
+//     explicit B(int b){
+//          std::cout << "Constructor called with "<< b<< std::endl;
+//      }
+// };
+// void foo(B x) { }
+//
+//  int main() {
+//      foo(5);
+//      return 0;
+//  }
+
+// #include <iostream>
+//
+// class B {
+// public:
+//     explicit B(int b){
+//         std::cout << "Constructor called with " << static_cast<int>(b) << std::endl;
+//     }
+// };
+// void foo(B x) { }
+//
+// int main() {
+//     foo(B(10));
+//     return 0;
+// }
+
+// #include <iostream>
+//
+// class B {
+// public:
+//     explicit B(int b){
+//         std::cout << "Constructor called with " << dynamic_cast<int>(b) << std::endl;
+//     }
+// };
+// void foo(B x) { }
+//
+// int main() {
+//     foo(B(5));
+//     return 0;
+// }
+
+// #include <iostream>
+//
+// class B {
+// public:
+//     explicit B(int b, float a){
+//         std::cout << "Constructor called with :" << static_cast<int>(b) << " , "<< static_cast<int>(a) <<std::endl;
+//     }
+// };
+//
+// int main() {
+//     B b {10,2.5};
+//     return 0;
+// }
+//
+
+#include <iostream>
+
+// class B {
+// public:
+//     explicit B(int b, float a){
+//         std::cout << "Constructor called with :" << static_cast<int>(b) << " , "<< static_cast<float>(a) <<std::endl;
+//     }
+// };
+//
+// int main() {
+//     B b {10,2.5};
+//     return 0;
+// }
